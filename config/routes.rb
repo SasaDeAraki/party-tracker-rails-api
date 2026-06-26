717e1eb66c793de0b19283f5f5fd31c7186baef4
Samuel Viana <samuel.viana.yl@gmail.com>
2026-06-26 15:37:29 -0300

    init: player controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,15 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :players do
+    resources :characters, only: [ :index, :show, :create ]
+    resources :tables, only: [ :index ]
+  end
+
+  resources :tables do
+    resources :characters, only: [ :index, :show ]
+    resources :players, only: [ :index ]
+  end
+
+  resources :characters
 end
