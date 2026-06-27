9b45cd230f070426952341a108b128dbba83d49d
Samuel Viana <samuel.viana.yl@gmail.com>
2026-06-27 12:09:03 -0300

    feat: character controller show and index

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,7 +2,13 @@ class CharactersController < ApplicationController
   def index
     set_player
 
-    render json: Player.find(@player.characters)
+    render json: @player.characters
+  end
+
+  def show
+    set_player
+
+    render json: @player.characters.find(params.extract_value(:id))
   end
 
   private
