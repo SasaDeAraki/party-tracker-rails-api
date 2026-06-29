26ff553d8e53527a759f1b5f7fbcf9cbf74d71c6
Samuel Viana <samuel.viana.yl@gmail.com>
2026-06-29 11:43:17 -0300

    feat: player create

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -11,9 +11,23 @@ class PlayersController < ApplicationController
     render json: @player
   end
 
+  def create
+    player = Player.new(player_params)
+
+    if player.save
+      render json: player, status: :created, location: player
+    else
+      render json: player.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_player
     @player = Player.find(params.extract_value(:id))
+  end
+
+  def player_params
+    params.expect(player: [ :name ])
   end
 end
